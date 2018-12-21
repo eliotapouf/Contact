bfc37486e90f113b8aa92873f12cafbb7bc9b4c3
eliotapouf <izzybadji@gmail.com>
2018-12-21 12:10:18 +0000

    dependances requises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Contacts} from '@ionic-native/contacts';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { CallNumber } from '@ionic-native/call-number';
+import { Calendar } from '@ionic-native/calendar';
+import { Camera } from '@ionic-native/camera';
+import { Firebase} from '@ionic-native/firebase';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,7 +30,13 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Contacts,
+    AndroidPermissions,
+    CallNumber,
+    Calendar,
+    Camera,
+    Firebase
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
+import { Contacts, ContactName, ContactField, ContactFieldType, ContactFindOptions } from '@ionic-native/contacts';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private contact: Contacts) {}
 
+  async getContacts() {
+    try{
+      const selectedContact = await this.contact.pickContact();
+      console.log(`Selected contact:`, selectedContact);
+    }catch(e){
+      console.error(e);
+    }
+  }
+
+  async addContact(){
+    try{
+      const newContact = this.contact.create();
+
+      newContact.name = new ContactName(null, 'Thiaka', 'Badji')
+      newContact.phoneNumbers = [new ContactField('mobile', '00221777739260')];
+
+      await newContact.save();
+
+      console.log(`New contact saved`, newContact);
+    }catch(e){
+      console.error(e);
+    }
+  }
+  
+
+  async findContact() {
+    try{
+      const options = new ContactFindOptions();
+      
+      options.filter = 'A Test Contact';
+      options.hasPhoneNumber = true;
+
+      const fields: ContactFieldType[] = ['name'];
+
+      const filteredContact = await this.contact.find(fields, options)
+      console.log(`Filtered contacts: `, filteredContact);
+    }catch(e){
+      console.error(e);
+    }
   }
 
 }
